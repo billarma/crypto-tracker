33d894763fdd57b0315369059761f76b7dae5b58
Ac1dBunny <49584855+Ac1dBunny@users.noreply.github.com>
2023-05-17 01:03:55 +0300

    Update tracker.js

--- tracker.js
+++ tracker.js
@@ -28,7 +28,7 @@ const main = async () => {
               }
 
             const formattedValueWithCommas = addCommas(formattedValue);
-            console.log(`$${formattedValueWithCommas} - Transferred`)
+            console.log(`$${formattedValueWithCommas} - Confirmed`)
             
             fs.appendFile('transactions.txt', `Transaction: https://etherscan.io/tx/${data.transactionHash} - $${formattedValueWithCommas}\n`, (err) => {
                 if (err) {
